997317da1fbf9ac76ed2ea4696a29d614a859f3a
Nick Caminiti <nmcams11@gmail.com>
2022-12-02 10:37:29 -0800

    Add functions for computer and user choices

---
+++ javascript.js
@@ -0,0 +1,52 @@
+// getComputerChoice randomly picks rock, paper, or scissor and stores in computerSelection
+
+function getComputerChoice() {
+    const randomNum = Math.random();
+    if (randomNum < 1/3) {
+        return "rock";
+    } else if (randomNum < 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+
+// ask for user input and store choice in variable playerSelection
+let playerSelection = prompt("Let's play Rock, Paper, Scissors.")
+//  use if statement to test if entry is valid
+testPlayerSelection()
+function testPlayerSelection(){
+    var regex = /^(rock|paper|scissors)$/i;
+    if (regex.test(playerSelection)) {
+
+    } else {
+        playerSelection = prompt("Invalid entry! Please enter Rock, Paper, or Scissors to play!");
+        testPlayerSelection();
+    }
+}
+
+// creat function playRound with inputs playerSelection and computerSelection
+//  find winner, return result i.e. "You Lose! Paper beats Rock"
+//  print result
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() == "ROCK") {
+        if (computerSelection == "rock") {
+            return "Tie!"
+        } else if (computerSelection == "paper") {
+            return "You Lose! Paper covers rock."
+        } else {
+            return "You Win! Rock smashes scissors."
+        }
+    } 
+}
+
+console.log(playRound(playerSelection,computerSelection))
+
+// create function game() that calls playRound()
+//  loop through the round 5 times
+//   store result in playerScore and computerScore
+//   print result and score at the end of each round
+//  print final score
