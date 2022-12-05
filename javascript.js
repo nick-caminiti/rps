a17017396b022f3b4fe2698a290d0fc4c7c4e332
Nick Caminiti <nmcams11@gmail.com>
2022-12-05 10:49:14 -0800

    Fix tie logic Tie's now log "tie" and only return the result of the tie breaker.

--- javascript.js
+++ javascript.js
@@ -1,66 +1,88 @@
-// getComputerChoice randomly picks rock, paper, or scissor and stores in computerSelection
+game()
 
+// create function game() that calls playRound()
+function game() {
+    for (let i = 0; i < 3; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Let's play Rock, Paper, Scissors.")
+        testPlayerSelection(playerSelection)
+        let userSCore
+        let computerScore
+        console.log("comp: " + computerSelection)
+        console.log("user: " + playerSelection)
+        console.log(playRound(playerSelection,computerSelection))
+
+    }
+    //return "gameover"
+}
+
+// getComputerChoice randomly picks rock, paper, or scissor and stores in computerSelection
 function getComputerChoice() {
     const randomNum = Math.random();
-    if (randomNum < 1/3) {
+    if (randomNum <= 1/3) {
         return "rock";
-    } else if (randomNum < 2/3) {
+    } else if (randomNum <= 2/3) {
         return "paper";
     } else {
         return "scissors";
     }
 }
-const computerSelection = getComputerChoice();
-console.log(computerSelection)
-
-// ask for user input and store choice in variable playerSelection
-let playerSelection = prompt("Let's play Rock, Paper, Scissors.")
 //  use if statement to test if entry is valid
-testPlayerSelection()
-function testPlayerSelection(){
+function testPlayerSelection(playerSelection) {
     var regex = /^(rock|paper|scissors)$/i;
     if (regex.test(playerSelection)) {
 
     } else {
         playerSelection = prompt("Invalid entry! Please enter Rock, Paper, or Scissors to play!");
-        testPlayerSelection();
+        testPlayerSelection(playerSelection);
     }
 }
-
-// creat function playRound with inputs playerSelection and computerSelection
+    // ask for user input and store choice in variable playerSelection
+// create function playRound with inputs playerSelection and computerSelection
 //  find winner, return result i.e. "You Lose! Paper beats Rock"
 //  print result
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() == "ROCK") {
         if (computerSelection == "rock") {
-            return "Tie!"
+            console.log("tie!")
+            return tie();
         } else if (computerSelection == "paper") {
-            return "You Lose! Paper covers rock."
+            return "userloss";
         } else {
-            return "You Win! Rock smashes scissors."
+            return "userwin";
         }
     } else if (playerSelection.toUpperCase() == "PAPER") {
         if (computerSelection == "rock") {
-            return "You Win! Paper covers rock."
+            return "userwin";
         } else if (computerSelection == "paper") {
-            return "Tie!"
+            console.log("tie!")
+            return tie();
         } else {
-            return "You lose! Scissors cuts paper."
+            return "userloss";
         }
     } else {
         if (computerSelection == "rock") {
-            return "You lose! Rock smashes scissors."
+            return "userloss";
         } else if (computerSelection == "paper") {
-            return "You win! Scissors cuts paper."
+            return "userwin";
         } else {
-            return "Tie!"
+            console.log("tie!")
+            return tie();
         }
     }
 }
-console.log(playRound(playerSelection,computerSelection))
 
-// create function game() that calls playRound()
+function tie() {
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Tie! Shoot again!");
+    testPlayerSelection(playerSelection);
+    console.log("comp: " + computerSelection);
+    console.log("user: " + playerSelection);
+    return playRound (playerSelection, computerSelection);;  
+}
+
+
 //  loop through the round 5 times
 //   store result in playerScore and computerScore
 //   print result and score at the end of each round
