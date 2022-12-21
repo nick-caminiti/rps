713b134cf68a6ae9f7f59c5eadd186a43d7218a5
Nick Caminiti <nmcams11@gmail.com>
2022-12-20 16:05:11 -0800

    Adding UI version

--- javascript.js
+++ javascript.js
@@ -1,22 +1,53 @@
-playGame()
+const buttonArea = document.querySelector('#buttonArea')
 
+const userSelectionDisplay = document.getElementById("userSelection")
+const CompSelectionDisplay = document.getElementById("CompSelection")
+const finalMessage = document.getElementById("finalMessage")
+const message = document.getElementById("message")
+const userScoreDisplay = document.getElementById("userScore")
+const compScoreDisplay = document.getElementById("compScore")
+
+let userScore = 0
+let computerScore = 0
+
+
+buttonArea.addEventListener('click', function (e) {
+    if (e.target.id == "rock" || e.target.id == "paper" || e.target.id == "scissors") {
+        if ((userScore + computerScore) == 5) {
+            userScore = 0
+            computerScore = 0
+            finalMessage.textContent = ''
+        }
+
+        let computerChoice = getComputerChoice()
+        let roundResult = playRound(e.target.id, computerChoice)
+        userSelectionDisplay.textContent = "You picked: " + e.target.id
+        CompSelectionDisplay.textContent = "The computer picked: " + computerChoice
+        userScoreDisplay.textContent = `Your Score: ${userScore}`;
+        compScoreDisplay.textContent =`Computer Score: ${computerScore}`;
+
+        if (roundResult == "userwin") {
+            userScore +=1;
+            userScoreDisplay.textContent = `Your Score: ${userScore}`;
+        } else if (roundResult == "userloss") {
+            computerScore +=1;
+            compScoreDisplay.textContent =`Computer Score: ${computerScore}`;
+        }
+        if ((userScore + computerScore) == 5) {
+            if (userScore > computerScore) {
+            finalMessage.textContent = `Game over! You Win!`;
+            } else {
+            finalMessage.textContent = `Game over! You Lose :(`;
+            }
+        }
+    }
+})
 // create function game() that calls playRound()
 function playGame() {
     //debugger
     let userScore = 0
     let computerScore = 0
     for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Let's play Rock, Paper, Scissors.")
-        if (testPlayerSelection(playerSelection)) {
-
-        } else {
-            playerSelection = prompt("Invalid entry! Please enter Rock, Paper, or Scissors to play!");
-        }
-        console.log("comp: " + computerSelection)
-        console.log("user: " + playerSelection)
-        let roundResult = playRound(playerSelection,computerSelection)
-        
         if (roundResult == "userwin") {
             userScore +=1;
             console.log("You win this round!");
@@ -65,42 +96,48 @@ function testPlayerSelection(playerSelection) {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() == "ROCK") {
         if (computerSelection == "rock") {
-            console.log("tie!");
             return tie();
         } else if (computerSelection == "paper") {
+            message.textContent = "You lose this round!"
             return "userloss";
         } else {
+            message.textContent = "You win this round!"
             return "userwin";
         }
     } else if (playerSelection.toUpperCase() == "PAPER") {
         if (computerSelection == "rock") {
+            message.textContent = "You win this round!"
             return "userwin";
         } else if (computerSelection == "paper") {
-            console.log("tie!");
             return tie();
         } else {
+            message.textContent = "You lose this round!"
             return "userloss";
         }
     } else {
         if (computerSelection == "rock") {
+            message.textContent = "You lose this round!"
             return "userloss";
         } else if (computerSelection == "paper") {
+            message.textContent = "You win this round!"
             return "userwin";
         } else {
-            console.log("tie!");
             return tie();
         }
     }
 }
 
 function tie() {
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt("Tie! Shoot again!");
-    testPlayerSelection(playerSelection);
-    console.log("comp: " + computerSelection);
-    console.log("user: " + playerSelection);
-    return playRound (playerSelection, computerSelection);
+    // let computerSelection = getComputerChoice();
+    message.textContent = "Tie! Shoot again!"
+    // testPlayerSelection(playerSelection);
+    // console.log("comp: " + computerSelection);
+    // console.log("user: " + playerSelection);
+    // return playRound (playerSelection, computerSelection);
 }
+
+
+
 
 
 //  loop through the round 5 times
